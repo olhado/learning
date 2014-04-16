a4838883c1972916394273032f5dacc837463ebf
Michael Chmielewski <code@mikec.123mail.org>
2014-04-15 23:04:44 -0400

    Converted tabs to spaces, solved Euler Problem #2

---
+++ euler2.ex
@@ -0,0 +1,39 @@
+defmodule Euler2 do
+  import Accumulator
+
+  def sum_even_fib(max_val) when is_integer(max_val) and max_val >= 1 do
+    sum_even_fib(max_val, 1, 0)
+  end
+
+  def sum_even_fib2(max_val) when is_integer(max_val) and max_val >= 1 do
+    sum_even_fib2(max_val, 3, 0)
+  end
+
+  defp sum_even_fib(max_val, current, acc) do
+    curr_fib = fib(current)
+    cond do
+      curr_fib <= max_val and rem(curr_fib, 2) != 0 ->
+        sum_even_fib(max_val, current + 1, acc)
+      curr_fib <= max_val and rem(curr_fib, 2) == 0 ->
+        sum_even_fib(max_val, current + 1, acc + curr_fib)
+      true ->
+        acc
+    end
+  end
+
+  defp sum_even_fib2(max_val, current, acc) do
+    curr_fib = fib(current)
+    cond do
+      curr_fib <= max_val ->
+        sum_even_fib2(max_val, current + 3, acc + curr_fib)
+      true ->
+        acc
+    end
+  end
+
+  def solve do
+    IO.puts inspect :timer.tc(Euler2, :sum_even_fib, [4000000])
+    IO.puts inspect :timer.tc(Euler2, :sum_even_fib2, [4000000])
+  end
+
+end
