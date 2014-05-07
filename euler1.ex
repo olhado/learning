e61427b44bdc9bbf43fb82deccfffb01b2d27bcb
Michael Chmielewski <code@mikec.123mail.org>
2014-05-06 20:55:09 -0400

    Added third solution for this, based on a friend's clojure solution.

--- euler1.ex
+++ euler1.ex
@@ -3,9 +3,16 @@ defmodule Euler1 do
   def solve do
     IO.puts inspect :timer.tc(Euler1, :sum_threes_and_fives, [1000])
     IO.puts inspect :timer.tc(Euler1, :lc_sum_threes_and_fives, [1000])
+    IO.puts inspect :timer.tc(Euler1, :map_reduce_sum_threes_and_fives, [1000])
   end
 
-def sum_threes_and_fives(x) when is_integer(x) and x > 0 do
+  def map_reduce_sum_threes_and_fives(a) when is_integer(a) and a > 0 do
+    1..a
+    |> Enum.filter(fn(x) -> rem(x, 5) == 0 or rem(x, 3) == 0 end)
+    |> Enum.reduce(fn(x, acc) -> acc + x end)
+  end
+
+  def sum_threes_and_fives(x) when is_integer(x) and x > 0 do
     range = 1..(x-1)
     Enum.filter(range, fn(num) -> threes_or_fives(num) end)
     |> Enum.reduce(fn(num, acc) -> num + acc end)
