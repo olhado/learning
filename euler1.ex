e1c4e116087645dbc546ce64d453c1f305961813
Michael Chmielewski <code@mikec.123mail.org>
2014-05-26 21:02:22 -0400

    Removed list comprehension version of Euler problem 1.
    Also removed some non-Euler code.

--- euler1.ex
+++ euler1.ex
@@ -2,35 +2,19 @@ defmodule Euler1 do
 	
   def solve do
     IO.puts inspect :timer.tc(Euler1, :sum_threes_and_fives, [1000])
-    IO.puts inspect :timer.tc(Euler1, :lc_sum_threes_and_fives, [1000])
     IO.puts inspect :timer.tc(Euler1, :map_reduce_sum_threes_and_fives, [1000])
   end
 
   def map_reduce_sum_threes_and_fives(a) when is_integer(a) and a > 0 do
     1..a
-    |> Enum.filter(fn(x) -> rem(x, 5) == 0 or rem(x, 3) == 0 end)
+    |> Enum.filter(fn(x) -> threes_or_fives(x) end)
     |> Enum.reduce(fn(x, acc) -> acc + x end)
   end
 
   def sum_threes_and_fives(x) when is_integer(x) and x > 0 do
-    range = 1..(x-1)
-    Enum.filter(range, fn(num) -> threes_or_fives(num) end)
+    1..x
+    |> Enum.filter(fn(num) -> threes_or_fives(num) end)
     |> Enum.reduce(fn(num, acc) -> num + acc end)
-  end
-
-  def lc_sum_threes_and_fives(x) when is_integer(x) and x > 0 do
-    the_range = span(1, x-1)
-    # Have to assign before use
-    values_to_sum = lc num inlist the_range, threes_or_fives(num) == true, do: num
-    Enum.reduce(values_to_sum, fn(num, acc) -> num + acc end)
-  end
-
-  defp span(from, to) when from > to do
-    []
-  end
-
-  defp span(from, to) do
-    [from | span(from + 1, to)]
   end
 
   defp threes_or_fives(x) when rem(x, 5) == 0 or rem(x, 3) == 0 do
--- tests.exs
+++
@@ -1,3 +0,0 @@
-Naive.fib 5
-
-Accumulator.fib 5
